39f0b79b30d74e4302d745b158b1b2039ff3f6ff
Cleyton Palauro <cleytonresc@gmail.com>
2019-09-18 19:24:07 -0300

    Add files via upload

---
+++ Scripts/DragScript.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScript : MonoBehaviour
+{
+    // Inserir o material de Physics do sprite bola.
+    public PhysicsMaterial2D PhysicsMat2D;
+
+    // Armazenando a posição do toque na tela.
+    private Vector2 touchPosition;
+
+    // Permitir que o sprite bola seja movido?
+    private bool moveAllowed = false;
+
+    // Referencia ao componente "Rigidbody2D".
+    private Rigidbody2D myRigidbody2D;
+
+    void Start()
+    {
+        // Referencia do componente.
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+
+        // Atribuição do PhysicsMaterial2D ao component Collider2D.
+        GetComponent<CircleCollider2D>().sharedMaterial = PhysicsMat2D;
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("touchCount: " + Input.touchCount);
+
+        // Iniciando o evento de toque.
+        if(Input.touchCount > 0)
+        {
+            // Armazenando a referencia do toque.
+            // Pegando o status do dedo que toca a tela.
+            Touch myTouch = Input.GetTouch(0);
+
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(
+                myTouch.position);
+            //Debug.Log("touchPos: " + touchPos);
+
+            switch (myTouch.phase)
+            {
+                // Usuário toca com o dedo na tela...
+                case TouchPhase.Began:
+                    Debug.Log("TouchPhase.Began");
+                    // Assim que o usuário tocar no GameObject...
+                    // SE a posição do toque sobrepor a posição do collider...
+                    if (GetComponent<Collider2D>() == 
+                            Physics2D.OverlapPoint(touchPos))
+                    {
+                        Debug.Log("Is overlap!");
+                        // Armazenar a posição em x,y para obter o...
+                        // ...delocamento entre a posição do toque e...
+                        // ... a posição do GameObject.
+                        touchPosition.x = touchPos.x - transform.position.x;
+                        touchPosition.y = touchPos.y - transform.position.y;
+
+                        // SE o toque for dentro do gameobject...
+                        // ...então será permitido mover com ele.
+                        moveAllowed = true;
+
+                        // Restringir algumas propriedade do Rigidbody2D.
+                        myRigidbody2D.freezeRotation = true;
+                        myRigidbody2D.velocity = new Vector2(0f, 0f);
+                        myRigidbody2D.gravityScale = 0f;
+
+                        // Remover o PhysicsMaterial2D do collider.
+                        GetComponent<CircleCollider2D>().sharedMaterial = null;  
+                    }
+                    else
+                    {
+                        Debug.Log("Overlap the screen only!");
+                    }
+
+                    break;
+                // Usuário move o dedo sobre a tela...
+                case TouchPhase.Moved:
+                    Debug.Log("TouchPhase.Moved");
+
+                    if (GetComponent<CircleCollider2D>() ==
+                            Physics2D.OverlapPoint(touchPos) && moveAllowed)  
+                    {
+                        // Atualizar a posição do meu GameObject.
+                        myRigidbody2D.MovePosition(new Vector2(
+                            touchPos.x - touchPosition.x,
+                            touchPos.y - touchPosition.y));
+                    }
+
+                    break;
+                // Usuário remove o dedo da tela...
+                case TouchPhase.Ended:
+                    Debug.Log("TouchPhase.Ended");
+
+                    // Não permitir mover o gameobject.
+                    moveAllowed = false;
+
+                    // Restaurar alguns parametros...
+                    myRigidbody2D.freezeRotation = false;
+                    myRigidbody2D.gravityScale = 2;
+
+                    GetComponent<CircleCollider2D>().sharedMaterial = PhysicsMat2D;  
+
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Scripts/PanoramicZoom.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanoramicZoom : MonoBehaviour
+{
+    // Armazernar a posição do toque na tela.
+    private Vector3 touchStart;
+
+    // O minimo de zoom.
+    [Range(0f, 10f)]
+    public float zoomMinimum = 1f;
+
+    // O máximo de zoom.
+    [Range(1f, 170f)]
+    public float zoomMaximum = 10f;
+
+    // Sensibilidade do zoom.
+    [Range(0f, 10f)]
+    public float zoomSensitivity = 1f;
+
+    void Start() { }
+
+    void Update()
+    {
+        // SE houver apenas 2 dedos sobre a tela...
+        if (Input.touchCount == 2)
+        {
+            Debug.Log("TWO Fingers!");
+            // Pegar o primeiro dedo que tocou na tela.
+            Touch touchOne = Input.GetTouch(0);
+
+            // Pegar o segundo dedo que tocou na tela.
+            Touch touchTwo = Input.GetTouch(1);
+
+            // A posição absoluta do primeiro toque.
+            Vector2 touchOnePreviousPos = 
+                touchOne.position - touchOne.deltaPosition;
+
+            // A posição absoluta do segundo toque.
+            Vector2 touchTwoPreviousPos = 
+                touchTwo.position - touchTwo.deltaPosition;
+
+            // Distancia absoluta anterior.
+            float previousMagnitude = (
+                touchOnePreviousPos - touchTwoPreviousPos).magnitude;
+
+            // Distancia absoluta atual.
+            float currentMagnitude = (
+                touchOne.position - touchTwo.position).magnitude;
+
+            // diferença entre as distancias.
+            float differenceMagnitude = 
+                currentMagnitude - previousMagnitude;
+
+            TouchZoom(differenceMagnitude * zoomSensitivity);
+        }
+        // SE houver apenas 1 dedo sobre a tela...
+        else if (Input.touchCount == 1)
+        {
+            Debug.Log("ONE Finger!");
+
+            // Pegar o dedo que tocou a tela.
+
+            // Verificar SE a TouhPhase está no começo...
+                // Atribuir a posição do toque na tela para coordenadas globais.
+
+            // Verificar SE a TouhPhase está movendo...
+                // Vector3 direction ==
+                // Atribuir a posição do toque na tela para coordenadas globais.
+
+                // Incrementar essa direção na camera.
+        }
+    }
+
+    private void TouchZoom(float increment)
+    {
+        // Se a camera for ortografica...
+        if (Camera.main.orthographic)
+        {
+            float orthoSize = Mathf.Clamp(
+                Camera.main.orthographicSize - increment,
+                zoomMinimum, zoomMaximum);
+
+            Camera.main.orthographicSize = orthoSize;
+        }
+        else // Se a camera for em perspectiva...
+        {
+            float fieldSize = Mathf.Clamp(
+                Camera.main.fieldOfView - increment,
+                zoomMinimum, zoomMaximum);
+
+            Camera.main.fieldOfView = fieldSize;
+        }
+    }
+}
